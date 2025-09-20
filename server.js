216e8aeb6398290b43d96cb7243c50cd10b6fb2f
mnhat28 <tranminhnhat20052805@gmail.com>
2025-09-20 10:03:28 +0700

    first commit with ci/cd

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 80;
+
+app.get('/', (req, res) => {
+  res.send("Hello from my distributed system!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
+
